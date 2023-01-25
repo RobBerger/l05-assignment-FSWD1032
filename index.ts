624265e883b680f92df50f73bd3cebbe8f1227c0
berger <robertberger.dev@gmail.com>
2023-01-25 18:56:50 -0500

    commented out all manager code to troubleshoot printSalary issues

--- index.ts
+++ index.ts
@@ -15,36 +15,37 @@ class Employee {
         return `$${this.salary}`;
     }
 
+
     printDetails(): string {
-        return `${this.name} is a ${this.title} in the ${this.department} department with a salary of ${this.printDetails}.`;
+        return `${this.name} is a ${this.title} in the ${this.department} department with a salary of ${this.printSalary}.`;
     }
 }
 
-class Manager extends Employee {
-    teamSize: number;
-    private readonly managerBonus: number = 1500;
+// class Manager extends Employee {
+//     teamSize: number;
+//     private readonly managerBonus: number = 1500;
 
-    constructor(empName: string, empTitle: string, empDepartment: string, empSalary: number, manTeamSize: number, manBonus: number) {
-        super(empName, empTitle, empDepartment, empSalary);
-        this.teamSize = manTeamSize;
-        this.managerBonus = manBonus;
-    }
+//     constructor(empName: string, empTitle: string, empDepartment: string, empSalary: number, manTeamSize: number, manBonus: number) {
+//         super(empName, empTitle, empDepartment, empSalary);
+//         this.teamSize = manTeamSize;
+//         this.managerBonus = manBonus;
+//     }
 
-    printDetails(): string {
-        return `${this.name} is a ${this.title} in the ${this.department} department with a salary of ${this.printDetails}. ${this.name} manages ${this.teamSize} employees.`;
-    }
-}
+//     printDetails(): string {
+//         return `${this.name} is a ${this.title} in the ${this.department} department with a salary of ${this.salary}. ${this.name} manages ${this.teamSize} employees.`;
+//     }
+// }
 
 let jimbo: Employee = new Employee('Jimbo', 'Lifeguard', 'Aquatics', 30000);
-let tammy: Manager = new Manager('Tammy', 'Aquatics Director', 'Aquatics', 50000, 7, 2000);
+// let tammy: Manager = new Manager('Tammy', 'Aquatics Director', 'Aquatics', 50000, 7, 2000);
 
 function printEmployee(callEmp: Employee) {
     console.log(callEmp.printDetails());
 }
 
-function printManager(callMan: Manager) {
-    console.log(callMan.printDetails());
-}
+// function printManager(callMan: Manager) {
+//     console.log(callMan.printDetails());
+// }
 
 printEmployee(jimbo);
-printManager(tammy);
+// printManager(tammy);
