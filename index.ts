3a7b09022beab9218c16ed06a7bf5df024ede29f
berger <robertberger.dev@gmail.com>
2023-01-25 17:33:32 -0500

    created class Employee

---
+++ index.ts
@@ -0,0 +1,21 @@
+class Employee {
+    protected name: string;
+    protected title: string;
+    protected department: string;
+    protected salary: number;
+
+    constructor(empName: string, empTitle: string, empDepartment: string, empSalary: number) {
+        this.name = empName;
+        this.title = empTitle;
+        this.department = empDepartment;
+        this.salary = empSalary;
+    }
+
+    printSalary(): string {
+        return `$${this.salary}`;
+    }
+
+    printDetails(): string {
+        console.log(`${this.name} is a ${this.title} in the ${this.department} with a salary of ${this.printDetails}.`);
+    }
+}
