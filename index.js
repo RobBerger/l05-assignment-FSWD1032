5ffa5be0490d9d75b4f8c182729cfe5cdbcbfb60
berger <robertberger.dev@gmail.com>
2023-01-25 19:02:08 -0500

    final test

--- index.js
+++ index.js
@@ -10,28 +10,27 @@ class Employee {
         return `$${this.salary}`;
     }
     printDetails() {
-        return `${this.name} is a ${this.title} in the ${this.department} department with a salary of ${this.printSalary}.`;
+        return `${this.name} is a ${this.title} in the ${this.department} department with a salary of $${this.salary}.`;
     }
 }
-// class Manager extends Employee {
-//     teamSize: number;
-//     private readonly managerBonus: number = 1500;
-//     constructor(empName: string, empTitle: string, empDepartment: string, empSalary: number, manTeamSize: number, manBonus: number) {
-//         super(empName, empTitle, empDepartment, empSalary);
-//         this.teamSize = manTeamSize;
-//         this.managerBonus = manBonus;
-//     }
-//     printDetails(): string {
-//         return `${this.name} is a ${this.title} in the ${this.department} department with a salary of ${this.salary}. ${this.name} manages ${this.teamSize} employees.`;
-//     }
-// }
+class Manager extends Employee {
+    constructor(empName, empTitle, empDepartment, empSalary, manTeamSize, manBonus) {
+        super(empName, empTitle, empDepartment, empSalary);
+        this.managerBonus = 1500;
+        this.teamSize = manTeamSize;
+        this.managerBonus = manBonus;
+    }
+    printDetails() {
+        return `${this.name} is a ${this.title} in the ${this.department} department with a salary of $${this.salary}. ${this.name} manages ${this.teamSize} employees.`;
+    }
+}
 let jimbo = new Employee('Jimbo', 'Lifeguard', 'Aquatics', 30000);
-// let tammy: Manager = new Manager('Tammy', 'Aquatics Director', 'Aquatics', 50000, 7, 2000);
+let tammy = new Manager('Tammy', 'Director', 'Aquatics', 50000, 7, 2000);
 function printEmployee(callEmp) {
     console.log(callEmp.printDetails());
 }
-// function printManager(callMan: Manager) {
-//     console.log(callMan.printDetails());
-// }
+function printManager(callMan) {
+    console.log(callMan.printDetails());
+}
 printEmployee(jimbo);
-// printManager(tammy);
+printManager(tammy);
--- index.ts
+++ index.ts
@@ -17,35 +17,35 @@ class Employee {
 
 
     printDetails(): string {
-        return `${this.name} is a ${this.title} in the ${this.department} department with a salary of ${this.printSalary}.`;
+        return `${this.name} is a ${this.title} in the ${this.department} department with a salary of $${this.salary}.`;
     }
 }
 
-// class Manager extends Employee {
-//     teamSize: number;
-//     private readonly managerBonus: number = 1500;
+class Manager extends Employee {
+    teamSize: number;
+    private readonly managerBonus: number = 1500;
 
-//     constructor(empName: string, empTitle: string, empDepartment: string, empSalary: number, manTeamSize: number, manBonus: number) {
-//         super(empName, empTitle, empDepartment, empSalary);
-//         this.teamSize = manTeamSize;
-//         this.managerBonus = manBonus;
-//     }
+    constructor(empName: string, empTitle: string, empDepartment: string, empSalary: number, manTeamSize: number, manBonus: number) {
+        super(empName, empTitle, empDepartment, empSalary);
+        this.teamSize = manTeamSize;
+        this.managerBonus = manBonus;
+    }
 
-//     printDetails(): string {
-//         return `${this.name} is a ${this.title} in the ${this.department} department with a salary of ${this.salary}. ${this.name} manages ${this.teamSize} employees.`;
-//     }
-// }
+    printDetails(): string {
+        return `${this.name} is a ${this.title} in the ${this.department} department with a salary of $${this.salary}. ${this.name} manages ${this.teamSize} employees.`;
+    }
+}
 
 let jimbo: Employee = new Employee('Jimbo', 'Lifeguard', 'Aquatics', 30000);
-// let tammy: Manager = new Manager('Tammy', 'Aquatics Director', 'Aquatics', 50000, 7, 2000);
+let tammy: Manager = new Manager('Tammy', 'Director', 'Aquatics', 50000, 7, 2000);
 
 function printEmployee(callEmp: Employee) {
     console.log(callEmp.printDetails());
 }
 
-// function printManager(callMan: Manager) {
-//     console.log(callMan.printDetails());
-// }
+function printManager(callMan: Manager) {
+    console.log(callMan.printDetails());
+}
 
 printEmployee(jimbo);
-// printManager(tammy);
+printManager(tammy);
